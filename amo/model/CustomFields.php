fad06026881b90b6f6589b9a84db54da28b51944
Даниил Кочура <kochura2017@yandex.ru>
2022-12-14 20:39:36 +0300

    Статический класс с идентификаторами полей заявки

---
+++ amo/model/CustomFields.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MzpoAmo;
+
+class CustomFields
+{
+	public const SITE = 639081;
+	public const CITY = 639087;
+	public const RESULT = 644675;
+	public const SOURCE = 639085;
+	public const TYPE = 639075;
+	public const LEAD1C = 710399;
+	public const COURSE = 357005;
+	public const ROISTAT = 639073;
+	public const PAGE = 639083;
+	public const ID_FORM = 644511;
+	public const YM_UID = 715049;
+	public const ROISTAT_MARKER = 645583;
+	public const EVENT_NAME = 725709;
+}
