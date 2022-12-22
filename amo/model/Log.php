c65fa5af0e205f9269060fb979894f7a33668bd9
Даниил Кочура <kochura2017@yandex.ru>
2022-12-22 15:57:30 +0300

    Настройки логирования

--- amo/model/Log.php
+++ amo/model/Log.php
@@ -4,9 +4,12 @@ namespace MzpoAmo;
 
 class Log
 {
-	public const CONTACT = 'contact.txt';
-	public const LEAD = 'lead.txt';
-	public const POST = 'common.txt';
+	public const CONTACT = 'contact/';
+	public const LEAD = 'lead/';
+	public const POST = 'common/';
+
+	public const COURSE = 'course/';
+
 	public const ERROR = 1;
 	public static function writeError($class, \Exception $exception = null)
 	{
@@ -15,11 +18,16 @@ class Log
 		$line = $exception->getLine();
 		$file = $exception->getFile();
 		$text = $exception->getMessage();
-		file_put_contents($_SERVER['DOCUMENT_ROOT'].'/amo/logs/' . $class, date("Y-m-d H:i:s") . ' : ' . $code . PHP_EOL . 'Line: ' . $line . ' File: ' . $file . PHP_EOL . 'Message: ' . $text . PHP_EOL . '_________________________' . PHP_EOL, FILE_APPEND);
+		file_put_contents($_SERVER['DOCUMENT_ROOT'].'/amo/logs/' . $class.date('Y-m').'.txt', date("Y-m-d H:i:s") . ' : ' . $code . PHP_EOL . 'Line: ' . $line . ' File: ' . $file . PHP_EOL . 'Message: ' . $text . PHP_EOL . '_________________________' . PHP_EOL, FILE_APPEND);
 	}
 	public static function write($class, $post)
 	{
-			file_put_contents($_SERVER['DOCUMENT_ROOT'].'/amo/logs/'.$class, date("Y-m-d H:i:s").' '.print_r($post, 1).PHP_EOL.'_________________________'.PHP_EOL, FILE_APPEND);
+			file_put_contents($_SERVER['DOCUMENT_ROOT'].'/amo/logs/'.$class.date('Y-m').'.txt', date("Y-m-d H:i:s").' '.print_r($post, 1).PHP_EOL.'_________________________'.PHP_EOL, FILE_APPEND);
+	}
+
+	public static function writeLine($class, $message)
+	{
+		file_put_contents($_SERVER['DOCUMENT_ROOT'].'/amo/logs/'.$class.date('Y-m').'.txt', date("Y-m-d H:i:s").': '.$message.PHP_EOL, FILE_APPEND);
 	}
 
 }
