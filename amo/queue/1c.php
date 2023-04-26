a5e8572f78d987ead8106ad346a84acbb443bae8
Даниил Кочура <kochura2017@yandex.ru>
2023-04-26 20:00:17 +0300

    Подписчик очереди для 1с: описание методов синхронизпции

--- amo/queue/1c.php
+++ amo/queue/1c.php
@@ -19,6 +19,7 @@ use services\QueueService;
 require_once $_SERVER['DOCUMENT_ROOT'].'/amo/model/MzpoAmo.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/amo/model/Leads.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/amo/model/Contact.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/amo/model/Company.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/amo/model/Log.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/amo/model/MzposApiEvent.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/amo/dict/CustomFields.php';
@@ -27,11 +28,70 @@ require_once $_SERVER['DOCUMENT_ROOT'].'/amo/dict/Tags.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/amo/dict/Statuses.php';
 require_once $_SERVER['DOCUMENT_ROOT'].'/amo/dict/Users.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/amo/services/QueueService.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/amo/services/Request1C.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/amo/services/Integartion1C.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/amo/services/UserService.php';
 require $_SERVER['DOCUMENT_ROOT'] .'/amo/reports/BaseReport.php';
 require $_SERVER['DOCUMENT_ROOT'] .'/amo/reports/EventsReport.php';
 require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
+require $_SERVER['DOCUMENT_ROOT'].'/amo/model/1C/Base1CInterface.php';
+require $_SERVER['DOCUMENT_ROOT'].'/amo/model/1C/Contact1C.php';
+require $_SERVER['DOCUMENT_ROOT'].'/amo/model/1C/Company1C.php';
+require $_SERVER['DOCUMENT_ROOT'].'/amo/model/1C/Lead1C.php';
+
+
+try {
+	#region создание соединения и слушателя очереди
+	$connection = new AMQPStreamConnection(QueueService::HOST, QueueService::PORT, QueueService::USER, QueueService::PASSWORD, QueueService::VHOST);
+
+	$channel = $connection->channel();
+	$channel->queue_declare(QueueService::INTEGRATION_1C, false, true, false, false);
+	#endregion
+
+	#region главный callback-бработчик
+	$callback = function ($msg){
+		$post = json_decode($msg->body, true);
+
+		$method = $post['method'];
+		Log::writeLine(Log::WEBHOOKS, print_r($post, 1));
+		Log::writeLine(Log::C1, print_r($post, 1));
+		file_put_contents(__DIR__.'/0.txt', print_r($post, 1), FILE_APPEND);
+		#region Инициализация мероприятий
+		if ($method == 'physics_corp') {
+			#region обработка запроса
+			$id = $post['leads']['add'][0]['id'] ?: $post['leads']['status'][0]['id'];
+			$status = $post['leads']['add'][0]['status_id'];
+			$pipeline = $post['leads']['add'][0]['pipeline_id'];
+			if (!$id) {
+				Log::writeError(Log::WEBHOOKS, new Exception('No id!'));
+			}
+			#endregion
+
+			$lead = new Leads([], MzpoAmo::SUBDOMAIN_CORP, $id, [\AmoCRM\Models\LeadModel::CATALOG_ELEMENTS, \AmoCRM\Models\LeadModel::CONTACTS]);
+//			Log::write(Log::C1, $lead->getLead());
+
+			$c1 = new \services\Integartion1C();
+			$c1->sendLead($lead);
+
+		}
+		#endregion
 
 
 
-//https://www.mzpo-s.ru/amo/webhooks/?ret2corp
-//https://mzpo2amo.ru/wh/ret2corp/send
+		$msg->ack();
+	};
+	#endregion
+
+	$channel->basic_consume(QueueService::INTEGRATION_1C, '', false, false, false, false, $callback);
+
+	while($channel->is_open())
+	{
+		$channel->wait();
+	}
+	$channel->close();
+	$connection->close();
+} catch (Exception $e) {
+	http_response_code(400);
+
+	file_put_contents(__DIR__.'/cons1.txt', print_r($e, 1).PHP_EOL);
+}
