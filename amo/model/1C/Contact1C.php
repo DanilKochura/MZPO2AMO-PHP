8779052189d7a623beca1e3cba182eafb0f6d1f7
Даниил Кочура <kochura2017@yandex.ru>
2023-04-28 20:10:34 +0300

    Модель сущности "Физическое лицо" с конвертером из амо

--- amo/model/1C/Contact1C.php
+++ amo/model/1C/Contact1C.php
@@ -16,6 +16,7 @@ class Contact1C implements Base1CInterface
 
 	public ?int $pass_serie;
 	public ?int $pass_number;
+	public ?int $pass_dpt_code;
 	public ?string $pass_issued_by;
 	public  $pass_issued_at;
 	public ?string $pass_dpt;
@@ -44,19 +45,23 @@ class Contact1C implements Base1CInterface
 
 	public static function fromAmo($contact, $type = null): Contact1C
 	{
+		//region __construct()
 		$client = new self();
+		//endregion
 
+		//region Получение модели контакта
 		if(!is_a($contact, 'MzpoAmo\Contact'))
 		{
 			$contact = new Contact([], $type, $contact);
 		}
+		//endregion
+
+		//region Заполнение полей модели
 		foreach (self::COMPARER as $prop => $amo) {
 			if (!empty($amo[$contact->getType()]) and property_exists(self::class, $prop)) {
 				$client->{$prop} = $contact->getCFValue($amo[$contact->getType()]);
 			}
 		}
-
-
 		$client->name = $contact->getName();
 
 		$client->amo_ids = [
@@ -73,6 +78,8 @@ class Contact1C implements Base1CInterface
 		{
 			$client->pass_issued_at = $client->pass_issued_at->format('d.m.Y G:i:s');
 		}
+		//endregion
+
 		return $client;
 	}
 
